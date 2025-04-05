ee78501cbb01d7d283c233e13124e8123aeb85a8
Sophie <sofi.sofi.01@mail.ru>
2025-04-05 15:04:36 +0300

    deleted:    public/file.svg

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div style={{ textAlign: 'center', fontSize: '40px' }}>
+        <p>404</p>
+        <p>Страница не найдена</p>
+      </div>
+    );
+  }
