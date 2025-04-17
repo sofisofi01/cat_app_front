40398870faf9cbeb4772075e08487b56d70dd714
s.zavyalova <s.zavyalova@vkteam.ru>
2025-04-17 22:56:10 +0300

    add page about styles

--- src/landings/about/index.tsx
+++ src/landings/about/index.tsx
@@ -1,19 +1,25 @@
 import styles from './about.module.scss'
 import { AboutPageProps } from './types' 
+import { Image } from '@/components/Image'
 import { ExtraHeader } from '@/components/ExtraHeader';
 
 
 export function AboutPage({title, caption, items, image}: AboutPageProps) {
     return (
     <div className={styles.wrapper}>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.caption}>{caption}</h2>
+        <Image {...image} className={styles.image}/>
+        <div className={styles.head}>
+            <h1 className={styles.title}>{title}</h1>
+            <h2 className={styles.caption}>{caption}</h2>
+        </div>
         <div className={styles.itemsWrapper}>
             {items && items.map((item)=>(
                 <div className={styles.item} key={item.id}>
                     <p className={styles.itemNum}>{item.id}</p>
-                    <h3 className={styles.itemTitle}>{item.title}</h3>
-                    <p className={styles.itemText}>{item.text}</p>
+                    <div>
+                        <h3 className={styles.itemTitle}>{item.title}</h3>
+                        <p className={styles.itemText}>{item.text}</p>
+                    </div>
                 </div>
                 ))}
         </div>
