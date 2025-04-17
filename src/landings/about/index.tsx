ef7efad48c8c01488a4e69c13c01da8cb1dcd329
s.zavyalova <s.zavyalova@vkteam.ru>
2025-04-17 20:22:08 +0300

    add content

--- src/landings/about/index.tsx
+++ src/landings/about/index.tsx
@@ -1,3 +1,11 @@
-export function AboutPage() {
-    return <div>About Page Content</div>;
+import styles from './about.module.scss'
+import { AboutPageProps } from './types' 
+
+
+export function AboutPage({title, caption, items, image}: AboutPageProps) {
+    return (
+    <div className={styles.wrapper}>
+        <h1 className={styles.title}>{title}</h1>
+    </div>
+    );
 }
