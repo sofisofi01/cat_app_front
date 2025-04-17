b1a21f3525dfef1c7ad1bf1ade2aee5f4e3703b4
s.zavyalova <s.zavyalova@vkteam.ru>
2025-04-17 20:40:47 +0300

    add mobile about page

--- src/landings/about/index.tsx
+++ src/landings/about/index.tsx
@@ -6,6 +6,16 @@ export function AboutPage({title, caption, items, image}: AboutPageProps) {
     return (
     <div className={styles.wrapper}>
         <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.caption}>{caption}</h2>
+        <div className={styles.itemsWrapper}>
+            {items && items.map((item)=>(
+                <div className={styles.item} key={item.id}>
+                    <p className={styles.itemNum}>{item.id}</p>
+                    <h3 className={styles.itemTitle}>{item.title}</h3>
+                    <p className={styles.itemText}>{item.text}</p>
+                </div>
+                ))}
+        </div>
     </div>
     );
 }
