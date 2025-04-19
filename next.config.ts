1505211add08f999e1e7437a45b8a82414033807
s.zavyalova <s.zavyalova@vkteam.ru>
2025-04-19 19:21:39 +0300

    add button fetch prediction

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://sofisofi01.pythonanywhere.com/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
--- src/landings/main/index.tsx
+++ src/landings/main/index.tsx
@@ -1,15 +1,30 @@
+"use client";
 import { ExtraHeader } from "@/components/ExtraHeader";
 import styles from "./main.module.scss";
 import { MainProps } from "./types";
 import { Image } from "@/components/Image";
+import { Prediction, PredictionService } from "@/services/api";
+import { useState } from "react";
 
 export function MainPage({ caption, btnText, background, cat }: MainProps) {
+  const [, setPrediction] = useState<Prediction | null>(null);
+
+  const fetchRandomPrediction = async () => {
+    try {
+      const randomPred = await PredictionService.getRandom();
+      setPrediction(randomPred);
+    } catch (error) {
+      console.error("Failed to fetch prediction:", error);
+    }
+  };
   return (
     <div className={styles.wrapper}>
       <Image {...background} className={styles.background} />
       <Image {...cat} className={styles.cat} />
       <h1 className={styles.caption}>{caption}</h1>
-      <button className={styles.btn}>{btnText}</button>
+      <button className={styles.btn} onClick={fetchRandomPrediction}>
+        {btnText}
+      </button>
       <ExtraHeader />
     </div>
   );
