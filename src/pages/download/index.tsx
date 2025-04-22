1a497db9a76ecc6d8f5506e5a87f5f12456fde12
d_d <d.v.cerveza@gmail.com>
2025-04-22 15:02:41 +0300

    download page

---
+++ src/pages/download/index.tsx
@@ -0,0 +1,8 @@
+import { DownloadPage } from "@/landings/download";
+import { DOWNLOAD_CONTENT } from "@/landings/download/const";
+
+const Download = () => {
+  return <DownloadPage {...DOWNLOAD_CONTENT} />;
+};
+
+export default Download;
