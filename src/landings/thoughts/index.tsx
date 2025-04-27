16f2f2d81c03e036af558949e9cdd79ba0d78c2b
s.zavyalova <s.zavyalova@vkteam.ru>
2025-04-27 14:13:03 +0300

    add books

--- src/landings/thoughts/index.tsx
+++ src/landings/thoughts/index.tsx
@@ -11,6 +11,11 @@ import { CommentsSection } from "@/components/CommentSection";
 import classNames from "classnames";
 import { ThoughtsProps } from "./types";
 import { useLikes } from "@/hooks/useLikes";
+import book1 from "./assets/book1.svg";
+import book2 from "./assets/book2.svg";
+import book3 from "./assets/book3.svg";
+import book4 from "./assets/book4.svg";
+import book5 from "./assets/book5.svg";
 
 export function ThoughtsPage({ usernameError, textError }: ThoughtsProps) {
   const {
@@ -155,6 +160,11 @@ export function ThoughtsPage({ usernameError, textError }: ThoughtsProps) {
           )}
         </div>
       )}
+      <Image className={styles.book} {...book1} />
+      <Image className={styles.book} {...book2} />
+      <Image className={styles.book} {...book3} />
+      <Image className={styles.book} {...book4} />
+      <Image className={styles.book} {...book5} />
       <ExtraHeader page={"thoughts"} />
     </div>
   );
